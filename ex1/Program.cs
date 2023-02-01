b03563e0a45c9bfce11c628774edac1d06c1c12b
kategitt <kapitanEka@yandex.ru>
2023-02-02 00:50:01 +0300

    Создала диаграмму по задаче, добавила рабочий код, поправила описание

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void methodArray (string [] array)
+{
+    string[] newarr = new string[array.Length];
+    for (int i=0; i<array.Length; i++)
+    {      
+        if(array[i].Length<=3)
+        {
+        newarr[i]=array[i];;
+        Console.WriteLine(newarr[i]); 
+        }
+     }
+        Console.WriteLine(); 
+}
+
+void Print(string [] Array)
+{
+    for (int i=0; i<Array.Length; i++)
+    {
+        Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine();
+}
+
+string [] array = {"hello", "2", "world", ":-)"};
+
+methodArray(array);
+Print(array);
